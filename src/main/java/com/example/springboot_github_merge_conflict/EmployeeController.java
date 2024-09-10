9b87c6d497c73b4381c66d25cf7966c9523fc98e
priyanka <priyankakushwaha7762@gmail.com>
2024-09-10 17:30:32 +0530

    updated

--- src/main/java/com/example/springboot_github_merge_conflict/EmployeeController.java
+++ src/main/java/com/example/springboot_github_merge_conflict/EmployeeController.java
@@ -1,5 +1,23 @@
 package com.example.springboot_github_merge_conflict;
 
-public class EmployeeController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class EmployeeController 
+{
+
+	@Autowired
+	EmployeeRepository repo;
+	
+	@PutMapping("/upd/{id}")
+	public String updateById(@RequestBody Employee emp,@PathVariable int id)
+	{
+		Employee e=repo.findById(id).get();
+		repo.save(e);
+		return "Data is updated";
+	}
 }
